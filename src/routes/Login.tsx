e7d93f88e918bbaca04e5cd7265bf8e7fdb1321b
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-19 01:26:18 +0800

    auth screens ux

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -1,7 +1,81 @@
-import React from "react";
+import {
+  Box,
+  Container,
+  Paper,
+  Button,
+  Link,
+  TextField,
+  Typography,
+  Avatar,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
 function Login() {
-  return <div>Login</div>;
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Container maxWidth="xs">
+        <Box
+          width="100%"
+          alignItems="center"
+          component={Paper}
+          elevation={3}
+          p={3}
+          display="flex"
+          flexDirection="column"
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <Box
+            component="form"
+            // onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Link href="#" variant="body2">
+              {"Don't have an account? Sign Up"}
+            </Link>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
 
 export default Login;
--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -1,7 +1,93 @@
-import React from "react";
+import {
+  Box,
+  Container,
+  Paper,
+  Button,
+  Link,
+  TextField,
+  Typography,
+  Avatar,
+  Grid,
+} from "@mui/material";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 
-function Signup() {
-  return <div>Signup</div>;
+function Login() {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      minHeight="100vh"
+    >
+      <Container maxWidth="xs">
+        <Box
+          width="100%"
+          alignItems="center"
+          component={Paper}
+          elevation={3}
+          p={3}
+          display="flex"
+          flexDirection="column"
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box component="form" noValidate sx={{ mt: 3 }}>
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="fullName"
+                  label="Full Name"
+                  name="fullName"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="new-password"
+                />
+              </Grid>
+            </Grid>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign Up
+            </Button>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="#" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </Box>
+  );
 }
 
-export default Signup;
+export default Login;
