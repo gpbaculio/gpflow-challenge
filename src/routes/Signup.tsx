ac9b594defe386c9f7c1125ec7a3c64156dd0fae
Glendon Philipp Baculio <gpbaculio@gmail.com>
2023-10-19 13:59:12 +0800

    form updates

--- src/routes/Signup.tsx
+++ src/routes/Signup.tsx
@@ -13,8 +13,28 @@ import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
 import { Link as RouterLink } from "react-router-dom";
 import { blue } from "@mui/material/colors";
 import FacebookIcon from "@mui/icons-material/Facebook";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
+
+const schema = yup.object().shape({
+  fullName: yup.string().required("Full Name is required"),
+  email: yup.string().email("Email is not valid").required("Email is required"),
+  password: yup
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .required("Password is required"),
+});
 
 function Signup() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   const handleFacebookLogin = () => {
     // Here you can handle the Facebook login
     console.log("Facebook login");
@@ -42,42 +62,58 @@ function Signup() {
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            sx={{ mt: 3 }}
+            onSubmit={handleSubmit((data) => {
+              console.log(data);
+            })}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12}>
                 <TextField
                   required
+                  onKeyDown={(e) => e.key === "Enter" && e.preventDefault()}
                   fullWidth
                   id="fullName"
                   label="Full Name"
-                  name="fullName"
+                  {...register("fullName")}
+                  error={!!errors.fullName}
+                  helperText={errors.fullName?.message}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  onKeyDown={(e) => e.key === "Enter" && e.preventDefault()}
                   required
                   fullWidth
                   id="email"
                   label="Email Address"
-                  name="email"
                   autoComplete="email"
+                  {...register("email")}
+                  error={!!errors.email}
+                  helperText={errors.email?.message}
                 />
               </Grid>
               <Grid item xs={12}>
                 <TextField
+                  onKeyDown={(e) => e.key === "Enter" && e.preventDefault()}
                   required
                   fullWidth
-                  name="password"
                   label="Password"
                   type="password"
                   id="password"
-                  autoComplete="new-password"
+                  {...register("password")}
+                  error={!!errors.password}
+                  helperText={errors.password?.message}
                 />
               </Grid>
             </Grid>
             <Button
               type="submit"
               fullWidth
+              onKeyDown={(e) => e.key === "Enter" && e.preventDefault()}
               variant="contained"
               sx={{ mt: 3, mb: 2 }}
             >
